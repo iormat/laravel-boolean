606e7fe23649cb8fc3852bde455979c30c73b3a4
iormat <iorio436@outlook.it>
2022-02-15 12:09:20 +0100

    basic project structure

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,16 @@
+<Header>
+    <h1>Postcards</h1>
+    {{-- login/register --}}
+    <div class="top-right links">
+        @auth
+            <a href="{{ url('/home') }}">Home</a>
+        @else
+            <a href="{{ route('login') }}">Login</a>
+
+            @if (Route::has('register'))
+                <a href="{{ route('register') }}">Register</a>
+            @endif
+        @endauth
+    </div>
+
+</Header>
